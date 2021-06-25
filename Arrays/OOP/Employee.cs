508531782812abc8d31a53782f0a115ff42288d9
Russel Sand <sand16031986@gmail.com>
2021-06-25 12:34:21 +0300

    test

---
+++ Arrays/OOP/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    abstract class Employee
+    {
+        private string name;
+        private double salary;
+        public abstract double CountSalary();
+    }
+}
